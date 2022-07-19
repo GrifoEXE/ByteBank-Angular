a27dfea03416bbf619014691ca98eaf9daf902d7
Pedro Figueiredo <pedroernestofsf@gmail.com>
2022-07-19 11:24:19 -0300

    Criação e Declaração de um Componente Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //declarações dos componentes pra aparecer na aplicação
+    AppComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule
