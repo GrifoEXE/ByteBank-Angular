e796cceb5cd7495441270bba16e069c79a412c81
Pedro Figueiredo <pedroernestofsf@gmail.com>
2022-07-20 09:01:29 -0300

    Criação Componente Extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     //declarações dos componentes pra aparecer na aplicação
     AppComponent,
     NovaTransferenciaComponent,
+    ExtratoComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
